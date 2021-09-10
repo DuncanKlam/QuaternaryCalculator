4015255444cba6e8dffbf9173f30e59c2444fa38
Spencer Naugler <spencernaugler7@gmail.com>
2021-09-10 19:03:53 -0400

    add business logic

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,37 @@
 public class Calculator {
+    public static void main(String[] args) {
+        String first = "102";
+        String second = "3";
+        System.out.println(add(first, second));
+    }
+    public static String add(String quaternary1, String quaternary2) {
+        int first = Integer.parseInt(quaternary1, 4);
+        int second = Integer.parseInt(quaternary2, 4);
+        return Integer.toString(first + second, 4);
+    }
+    public String sub(String quaternary1, String quaternary2) {
+        int first = Integer.parseInt(quaternary1, 4);
+        int second = Integer.parseInt(quaternary2, 4);
+        return Integer.toString(first - second, 4);
+    }
+    public String mul(String quaternary1, String quaternary2) {
+        int first = Integer.parseInt(quaternary1, 4);
+        int second = Integer.parseInt(quaternary2, 4);
+        return Integer.toString(first * second, 4);
+    }
+    public String div(String quaternary1, String quaternary2) {
+        int first = Integer.parseInt(quaternary1, 4);
+        int second = Integer.parseInt(quaternary2, 4);
+        return Integer.toString(first / second, 4);
+    }
+
+    public String root(String quaternary) {
+        int decimal = Integer.parseInt(quaternary, 4);
+        return Integer.toString((int) Math.sqrt(decimal), 4);
+    }
+
+    public String square(String quaternary) {
+        int decimal = Integer.parseInt(quaternary, 4);
+        return Integer.toString((int) Math.pow(decimal, 2), 4);
+    }
 }
