67c3f38fe432c0798e92677d18e00bdb79be0f43
Ben Eger <bteger28@gmail.com>
2021-09-11 13:28:34 -0400

    Add testAddZeros()

---
+++ test/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAddZeros() {
+        String expected = "0";
+        Assertions.assertEquals(expected, Calculator.add("0","0"));
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void root() {
+    }
+
+    @Test
+    void square() {
+    }
+}
