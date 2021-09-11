a7c589403b384bb7597fa87d8f7d84d0a5accd4e
Ben Eger <bteger28@gmail.com>
2021-09-11 14:30:37 -0400

    Add testAddVariousNums()

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -9,6 +9,21 @@ public class CalculatorTest {
     }
 
     @Test
+    public void testAddVariousNums() {
+        String[] expected = {"3", "10", "23", "333", "1000", "321321"};
+
+        String[] actual = new String[6];
+        actual[0] = Calculator.add("1", "2");
+        actual[1] = Calculator.add("2", "2");
+        actual[2] = Calculator.add("11", "12");
+        actual[3] = Calculator.add("123", "210");
+        actual[4] = Calculator.add("333", "1");
+        actual[5] = Calculator.add("321321", "0");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void testAddLargeNums() {
         String expected = "13333332";
         String largeNum = "3333333";
@@ -21,6 +36,7 @@ public class CalculatorTest {
 
     @Test
     void mul() {
+
     }
 
     @Test
