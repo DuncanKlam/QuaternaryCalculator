0295da1d667600b6293a01208813700a257ea35e
Ben Eger <bteger28@gmail.com>
2021-09-11 13:25:40 -0400

    Change calculator methods to static

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,28 +4,28 @@ public class Calculator {
         int second = Integer.parseInt(quaternary2, 4);
         return Integer.toString(first + second, 4);
     }
-    public String sub(String quaternary1, String quaternary2) {
+    public static String sub(String quaternary1, String quaternary2) {
         int first = Integer.parseInt(quaternary1, 4);
         int second = Integer.parseInt(quaternary2, 4);
         return Integer.toString(first - second, 4);
     }
-    public String mul(String quaternary1, String quaternary2) {
+    public static String mul(String quaternary1, String quaternary2) {
         int first = Integer.parseInt(quaternary1, 4);
         int second = Integer.parseInt(quaternary2, 4);
         return Integer.toString(first * second, 4);
     }
-    public String div(String quaternary1, String quaternary2) {
+    public static String div(String quaternary1, String quaternary2) {
         int first = Integer.parseInt(quaternary1, 4);
         int second = Integer.parseInt(quaternary2, 4);
         return Integer.toString(first / second, 4);
     }
 
-    public String root(String quaternary) {
+    public static String root(String quaternary) {
         int decimal = Integer.parseInt(quaternary, 4);
         return Integer.toString((int) Math.sqrt(decimal), 4);
     }
 
-    public String square(String quaternary) {
+    public static String square(String quaternary) {
         int decimal = Integer.parseInt(quaternary, 4);
         return Integer.toString((int) Math.pow(decimal, 2), 4);
     }
